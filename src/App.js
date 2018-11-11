df3d1b77533b832cc2442c17ae7677bc4650f87f
praveen-me <praveenkrsaini0@gmail.com>
2018-11-11 12:42:13 +0530

    Convert Stylus into scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Main from './components/Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Router>
+          <Route path="/" exact component={Main} />
+        </Router>
       </div>
     );
   }
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import css from  './../styles/style.scss';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">Reduxtagram</Link>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
